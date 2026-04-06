50f144b8a1e202e3c85edf6e90432af85ec4e981
Vijay Prajapati <vijayprajapati71234@gmail.com>
2026-04-07 00:50:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 from langchain_groq import ChatGroq
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_objectbox.vectorstores import ObjectBox
